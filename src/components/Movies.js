6ccb06851343cbb66ba5ac9bbc89826709555f47
Chrispus <skilochris@gmail.com>
2023-11-20 16:16:06 +0300

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,31 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map(movie => {
+        return (
+          <div key={Math.floor(Math.random() * 10000)}>
+            <h2>{movie.title}</h2>
+            <p>Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>
+              {movie.genres.map(genre => {
+                return (
+                  <li key={genre}>
+                    {genre}
+                  </li>
+                )
+              })}
+            </ul>
+            </div>
+        )
+      })}
+    
+    
+    
+    
+    {/*{code here}*/}</div>;
 }
 
 export default Movies;
